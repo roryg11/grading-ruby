13e4f4cbb4d1b3779dceae97fa0a13f8575b841d
roryg11 <Rory.C.Grant@gmail.com>
2014-12-16 09:56:10 -0800

    2nd challenge and changing into methods

--- lib/grade_difference.rb
+++ lib/grade_difference.rb
@@ -1,16 +1,48 @@
 bryon = [18, 57, 91, 63, 17, 36, 87, 40, 38, 40, 43, 57]
 
-i=0
+ivy = [85, 84, 86, 14, 14, 26, 92, 36, 95, 84, 67, 46]
 
-grade_differences= []
-
-while grade_differences.length < 11
-  if bryon[i] > bryon[i + 1]
-    grade_differences.push(:down)
-  elsif bryon[i] < bryon[i + 1]
-    grade_differences.push(:up)
+def grade_difference(student)
+  i = 0
+  array=[]
+  while array.length < 11
+    if student[i] > student[i + 1]
+      array.push(:down)
+    elsif student[i] < student[i + 1]
+      array.push(:up)
+    elsif student[i] = student[i+1]
+      array.push(:even)
+    end
+    i= i +1
   end
-  i= i +1
+  array
 end
 
-puts grade_differences
+def decline?(student)
+  student_grade = grade_difference(student).count(:down)
+  if student_grade >=3 && grade_difference(student)[-1] != :up
+    true
+  else
+    false
+  end
+end
+
+puts grade_difference(ivy)
+
+puts decline?(ivy)
+
+# def decline?(student)
+#   student_grade = grade_difference(student)
+#   var_occur = array.each_with_object(Hash.new(0)) {|var, count| count[var] += 1}
+#   if array[-1] == :up
+#     false
+#   elsif
+#     if var_occur[:down] >= 3
+#       true
+#     else
+#       false
+#     end
+#   else
+#     false
+#   end
+# end
